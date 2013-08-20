5a96b89f399ae0eeb56e68bdf7599194b1c5cb95
Ryan Whitley <rwhitley@spatialdev.com>
2013-08-20 13:59:46 -0700

    added back in session cookie parser

--- GeoWebServices.js
+++ GeoWebServices.js
@@ -43,6 +43,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
